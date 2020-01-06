58968e6bc91fcacf092713ac103e73339eed6f2c
unknown <jgj_junior@yahoo.com.br>
2020-01-05 22:25:51 -0300

    production

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(port));
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build"))); // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.get("/backend", (req, res) => {
   res.send({ express: "connected" });
 });
